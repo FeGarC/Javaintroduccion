9d67da924e579836d5d7230edfb14f1fa01228c4
fegar <fegarceo9606@gmail.com>
2023-03-28 14:14:30 -0500

    Ejercicio de introducción en Java

---
+++ src/javaintroduccion/JavaIntroduccion.java
@@ -0,0 +1,22 @@
+package javaintroduccion;
+
+public class JavaIntroduccion {
+
+    
+    public static void main(String[] args) {
+        String nombre = "Felipe";
+        boolean flag = true;
+        char vocal = 'a';
+        int numero = 48;
+        double decimales = 3.55;
+        boolean bandera = false;
+        
+        System.out.println(nombre);
+        System.out.println(flag);
+        System.out.println(vocal);
+        System.out.println(numero);
+        System.out.println(decimales);
+        System.out.println(bandera);
+    }
+    
+}
